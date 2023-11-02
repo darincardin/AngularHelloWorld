6764ec313b761ac0bdb9fa1057e564c3050e371f
Darin Cardin <darinjaycardin@gmail.com>
2023-11-02 14:18:08 -0400

    Create app.spec.ts

---
+++ src/app/app.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, async } from '@angular/core/testing';
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+
+    expect(true).toBeTruthy();
+  }));
+
+});
