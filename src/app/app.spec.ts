09dfc9deeb48cf06dcaa29a6848b944f358271f1
Darin Cardin <darinjaycardin@gmail.com>
2023-11-02 15:42:23 -0400

    Update app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,27 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        
+        AppComponent
       ],
     }).compileComponents();
   }));
   
-  it('should create the app', async(() => {
-    expect(true).toBeTruthy();
-  }));
-
-  it('should create the app', async(() => {
-    expect(true).toBeTruthy();
-  }));
-
- it('should create the app', async(() => {
-    expect(false).toBeTruthy();
-  }));
-
-
-  it('should create the app', async(() => {
-    expect(true).toBeTruthy();
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Darin');
   }));
 });
