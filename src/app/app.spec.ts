3daa30c62c5261b0885a6980fe3c925d95fed589
Darin Cardin <darinjaycardin@gmail.com>
2023-11-02 15:46:20 -0400

    Update app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -12,6 +12,6 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Darin');
+    expect(app.title).toEqual('AAAAA_Darin');
   }));
 });
